c795d8c3f02a4ee4ef9aad49352a77443175e58a
Joao Santos <jrmsantos15@gmail.com>
2019-11-18 12:32:03 +0000

    fixes

--- src/pages/Decode.js
+++ src/pages/Decode.js
@@ -2,8 +2,7 @@ import React, {useState} from 'react'
 import {FlagWord} from '../components/FlagWord'
 
 export const Decode = () => {
-  const [message, setMessage] = useState('a')
-
+  const [message, setMessage] = useState('')
   const updateInput = (e) => {
     setMessage(e.target.value)
   }
@@ -11,11 +10,9 @@ export const Decode = () => {
   return (
     <div className='decode-wrapper'>
       <div className='decode-input'>
-        <p>Enter your message</p>
-        <input type='text' onChange={updateInput} />
+        <input type='text' onChange={updateInput} placeholder='Your message here' />
       </div>
       <div className='decode-output'>
-        <p>{message}</p>
         <FlagWord word={message} />
       </div>
 
